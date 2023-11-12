3358f48a3686e57b2c922374ec49600e986f073f
nguyenquockhang1610 <nguyenquockhang1610@gmail.com>
2023-11-12 19:17:06 +0700

    finalllll

--- null_destoy.tf
+++ null_destoy.tf
@@ -8,8 +8,6 @@ resource "null_resource" "destroy" {
     when        = destroy
     interpreter = ["/bin/bash", "-c"]
     command     = <<EOT
-        #echo "Remove helm deployment"
-        #helm delete aws-load-balancer-controller -n kube-system
         if [ -f "crds.yml" ]; then
         echo "Set context"
         arn=$(terraform state show aws_eks_cluster.cluster | grep arn | grep :cluster | cut -f2 -d'=' | jq -r .)
